296cfc704c45b88e9a16a928aab2625fc773457a
Fehmauri <felipe-mauri@hotmail.com>
2024-08-11 20:12:00 -0300

    Creating an API with ASP.NET Identity

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using ApiFuncional.Configuration;
+using ApiFuncional.Data;
+using ApiFuncional.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder
+    .AddApiConfig()
+    .AddCorsConfig()
+    .AddSwaggerConfig()
+    .AddDbContextConfig()
+    .AddIdentityConfig();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors("Development");
+}
+else
+{
+    app.UseCors("Production");
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
